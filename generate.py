b4738378e00faf2bcbe3fd9c17d1ca8a23d67cce
Matjaž Horvat <matjaz.horvat@gmail.com>
2015-10-05 09:13:29 +0200

    Add Romanian (ro)

--- generate.py
+++ generate.py
@@ -6,8 +6,8 @@ import sys
 possible_locales = [
     'ca', 'cs', 'cy', 'da', 'de', 'dsb', 'en', 'es', 'es_AR', 'es_CL',
     'et', 'eu', 'ff', 'fr', 'fy', 'he', 'hsb', 'hu', 'id', 'it', 'ja', 'ko',
-    'lt', 'nb_NO', 'nl', 'pa', 'pl', 'pt', 'pt_BR', 'rm', 'ru', 'sk', 'sl',
-    'sq', 'sr', 'sr_Latn', 'sv', 'sv_SE', 'tr', 'uk', 'zh_CN', 'zh_TW'
+    'lt', 'nb_NO', 'nl', 'pa', 'pl', 'pt', 'pt_BR', 'rm', 'ro', 'ru', 'sk',
+    'sl', 'sq', 'sr', 'sr_Latn', 'sv', 'sv_SE', 'tr', 'uk', 'zh_CN', 'zh_TW'
 ]
 
 # For the moment, no locales are actually ready, as they become translated,
